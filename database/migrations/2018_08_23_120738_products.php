464b5e6169db8f42943cfb585e12f665493f075b
KoketsoMabuela92 <glenton92@gmail.com>
2018-08-23 19:24:32 +0200

    DATABASE: review migrations

--- database/migrations/2018_08_23_120738_products.php
+++ database/migrations/2018_08_23_120738_products.php
@@ -4,30 +4,28 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class MorecorpProducts extends Migration
+class Products extends Migration
 {
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
-    {
+    public function up() {
 
         if (!Schema::connection('mysql')->hasTable('products')) {
             Schema::connection('mysql')
                 ->create(
                     'products',
                     function (Blueprint $table) {
-                        $table->engine = 'InnoDB';
-                        $table->charset = 'latin1';
-                        $table->collation = 'latin1_swedish_ci';
-
-                        $table->integer('id')->increments(1)->primary();
+                        $table->integer('id')->autoIncrement();
                         $table->string('name',50);
                         $table->string('sku',50)->unique();
                         $table->double('price');
                         $table->string('description', 50);
+                        $table->string('fullPath');
+                        $table->string('mimeType');
+                        $table->integer('viewCount')->default(0);
                         $table->timestamps();
                     }
                 );
@@ -39,8 +37,8 @@ class MorecorpProducts extends Migration
      *
      * @return void
      */
-    public function down()
-    {
+    public function down() {
+
         DB::statement('SET FOREIGN_KEY_CHECKS = 0');
         Schema::connection('mysql')->dropIfExists('products');
         DB::statement('SET FOREIGN_KEY_CHECKS = 1');
--- database/migrations/2018_08_23_120744_products_bids.php
+++ database/migrations/2018_08_23_120744_products_bids.php
@@ -4,27 +4,27 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class ProductBids extends Migration
+class ProductsBids extends Migration
 {
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
-    {
+    public function up() {
         if (!Schema::connection('mysql')->hasTable('product_bids')) {
             Schema::connection('mysql')
                 ->create(
                     'product_bids',
                     function (Blueprint $table) {
-                        $table->engine = 'InnoDB';
-                        $table->charset = 'latin1';
-                        $table->collation = 'latin1_swedish_ci';
-
-                        $table->integer('id')->increments(1)->primary();
-                        $table->foreign('productId')->references('products')->on('id');
+                        $table->integer('id')->autoIncrement();
+                        $table->integer('productId');
+                        $table->float('highestBid')->default(0.00);
+                        $table->float('lowestBid')->default(0.00);
+                        $table->float('averageBid')->default(0.00);
+                        $table->integer('bidCount')->default(0);
                         $table->timestamps();
+                        $table->foreign('productId')->references('id')->on('products');
                     }
                 );
         }
@@ -35,8 +35,8 @@ class ProductBids extends Migration
      *
      * @return void
      */
-    public function down()
-    {
+    public function down() {
+
         DB::statement('SET FOREIGN_KEY_CHECKS = 0');
         Schema::connection('mysql')->dropIfExists('product_bids');
         DB::statement('SET FOREIGN_KEY_CHECKS = 1');
