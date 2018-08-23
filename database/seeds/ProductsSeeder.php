a1c1808a778182a82f15a9a4f8aed0866e0a2dd0
KoketsoMabuela92 <glenton92@gmail.com>
2018-08-23 19:35:00 +0200

    DATABASE: products seeder

--- database/seeds/ProductsSeeder.php
+++ database/seeds/ProductsSeeder.php
@@ -8,36 +8,43 @@ class ProductsSeeder extends Seeder
     /**
      * Run the database seeds.
      */
+
     public function run()
     {
         $date = Carbon::now()->toDateTimeString();
 
         DB::connection('mysql')->table('products')->insert([
-            'id' => 1,
             'name' => 'Cellphone X',
             'sku' => '1111111111',
             'price' => 2000,
             'description' => 'Cellphone X test description',
+            'fullPath' => '/var/www/html/more_corp_test/product_pictures/1.png',
+            'mimeType' => 'png',
+            'viewCount' => 0,
             'created_at' => $date,
             'updated_at' => $date,
         ]);
 
         DB::connection('mysql')->table('products')->insert([
-            'id' => 2,
             'name' => 'Cellphone Y',
             'sku' => '2222222222',
             'price' => 6000,
             'description' => 'Cellphone Y test description',
+            'fullPath' => '/var/www/html/more_corp_test/product_pictures/2.png',
+            'mimeType' => 'png',
+            'viewCount' => 0,
             'created_at' => $date,
             'updated_at' => $date,
         ]);
 
         DB::connection('mysql')->table('products')->insert([
-            'id' => 3,
             'name' => 'Cellphone Z',
             'sku' => '3333331',
             'price' => 2000,
             'description' => 'Cellphone Z test description',
+            'fullPath' => '/var/www/html/more_corp_test/product_pictures/3.png',
+            'mimeType' => 'png',
+            'viewCount' => 0,
             'created_at' => $date,
             'updated_at' => $date,
         ]);
